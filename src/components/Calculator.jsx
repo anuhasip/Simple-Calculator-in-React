b18e4787ae8fc870789b99d7af74c07d23942e2b
Anuhas Paranavithana <anuhasindumina@gmail.com>
2023-07-21 19:44:25 +0530

    Add Functionality to Calculator

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,11 +1,9 @@
 import { useState } from "react";
 
-function Display({displayText, displayAnswer}) {
+function Display({displayText}) {
     return ( 
         <div className="display">
-            {displayText}
-            <br />
-            <span className="answer-area">{displayAnswer}</span>
+            <span>{displayText}</span>
         </div>
      );
 }
@@ -14,22 +12,56 @@ function Display({displayText, displayAnswer}) {
 function Calculator() {
 
     const [displayText, setDisplayText] = useState("");
-    const [displayAnswer, setDisplayAnswer] = useState("0");
 
-    const handleClickNumber = (num) => {
-        setDisplayText((prevValue) => prevValue + num)
+    const handleClickButton = (op) => {
+        if(displayText == "Invalid" || displayText == "Infinity"){
+            setDisplayText("");
+        }
+        
+        switch(op){
+            case "0":case "1": case "2": case "3": case "4": case "5": case "6": case "7": case "8": case "9":
+                setDisplayText((prevValue) => prevValue + op);
+                return ;
+            case "=":
+                if(displayText.length == 0) return ;
+                try{
+                    let answer = eval(displayText);
+                    if(String(answer).length > 15) answer = String(answer).substring(0, 16);
+                    setDisplayText(String(answer));
+                }
+                catch{
+                    setDisplayText("Invalid");
+                }
+                return ;
+            case "+": case "-": case "*": case "/": case ".":
+                if("+-*/.".includes(displayText[displayText.length - 1])) return ;
+                if(displayText.length == 0) return ;
+                setDisplayText((prevValue) => prevValue + op);
+                return ;               
+        }
+    }
+
+    const allClear = () => {
+        setDisplayText("")
+    }
+
+    const lastClear = () => {
+        if(displayText == "Invalid" || displayText == "Infinity"){
+            setDisplayText("");
+        }
+        setDisplayText(prevValue => prevValue.substring(0, prevValue.length - 1));
+        
     }
 
     const ButtonNum = (n) => {
-        return <button className="cal-btn" onClick={ () => handleClickNumber(n) }> { n } </button>
+        return <button className="cal-btn" onClick={ () => handleClickButton(n) }> { n } </button>
     }
 
     return ( 
         <div className="calculator">
-            <Display displayText={displayText} displayAnswer={displayAnswer} />
-            { ButtonNum("AC") }
-            { ButtonNum("C") }
-            { ButtonNum("%") }
+            <Display displayText={displayText} />
+            <button className="cal-btn cal-ac-btn" onClick={ allClear }>AC</button>
+            <button className="cal-btn cal-c-btn" onClick={ lastClear }>C</button>
             { ButtonNum("/") }
             <br/>
             { ButtonNum("7") }
@@ -47,10 +79,9 @@ function Calculator() {
             { ButtonNum("3") }
             { ButtonNum("+") }
             <br/>
-            { ButtonNum("+-") }
             { ButtonNum("0") }
             { ButtonNum(".") }
-            { ButtonNum("=") }
+            <button className="cal-btn cal-equal-btn" onClick={ () => handleClickButton("=") }>=</button>
             <br/>
         </div>
      );
